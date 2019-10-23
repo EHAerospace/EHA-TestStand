5b0011493574ba5962f0d4aeae7d3ac4a5a53ba2
mikelma <mikelma7@gmail.com>
2019-10-23 19:08:59 +0200

    Files adapted to use platformio

--- logger/src/logger.ino
+++ logger/src/logger.ino
@@ -15,13 +15,16 @@
  	Buzzer - pin 6
 */
 
+#define RECORD_SECONDS 30
+#define AFT_SIGNAL_DELAY_SECS 7
+#define BUZZ_FREQUENCY 440
+
 #define PIN_ACTIVATION_SIGNAL 4
 #define PIN_LED 5
-#define PIN_BUZZ 6
+#define PIN_BUZZ 7
 #define PIN_POWER 8
 #define PIN_CLOCK	2
 #define PIN_DATAOUT	3
-#define BUZZ_FREQUENCY 440
 
 // Part of the SD pins
 #define PIN_SDCS 10
@@ -40,16 +43,19 @@ String get_filename()
 void setup_peripherals()
 {
 	// Setup the Serial
-	Serial.begin(38400);
+	Serial.begin(9600);
+
+        Serial.println("EH Launcher ON");
+
 	while (!Serial);
 	Serial.println("Serial initialized.");
 
 	// Setup SD card
 	Serial.print("Initializing SD card...");
-	if (!SD.begin(PIN_SDCS))
+        while (!SD.begin(PIN_SDCS))
 	{
-		Serial.println(" Failed!");
-		return;
+		Serial.println("SD Failed!");
+                make_signals(5, 50);
 	}
 	Serial.println("Done.");
 
@@ -68,10 +74,11 @@ void make_signals(int signal_count, int interval)
 	for (int f = 0; f < signal_count; f++)
 	{
 		digitalWrite(PIN_LED, HIGH);
-		tone(PIN_BUZZ, BUZZ_FREQUENCY, 500);
+		tone(PIN_BUZZ, BUZZ_FREQUENCY, interval/2);
+		delay(interval/2);
 		digitalWrite(PIN_LED, LOW);
 		noTone(PIN_BUZZ);
-		delay(interval);
+		delay(interval/2);
 	}
 }
 
@@ -84,14 +91,15 @@ void take_samples(HX711& scale, File& file, int sample_count)
 	}
 }
 
+
+
 void setup() 
 {
+        setup_peripherals();
+
 	// Initializes HX711
 	HX711 scale(PIN_DATAOUT, PIN_CLOCK);
   
-        // Sets up the peripherals 
-	setup_peripherals();
-
 	// Prepare the scale and log the calculated scale factor to the serial
 	Serial.println("Calibrating sensor...");
 	scale.set_scale();
@@ -119,9 +127,16 @@ void setup()
 
 	// Wait until the activation signal is on, and take the specified samples
 	Serial.println("Waiting until the start signal is on...");
+        make_signals(10, 250);
+        
 	while(digitalRead(PIN_ACTIVATION_SIGNAL) == LOW);
-	Serial.println("Signal recieved, starting sampling routine");
-	take_samples(scale, file, 800);
+        Serial.println("Signal received, waiting delay...");
+        make_signals(10, 100);
+
+        delay(AFT_SIGNAL_DELAY_SECS*1000);
+
+	Serial.println("Starting sampling routine");
+	take_samples(scale, file, 80*RECORD_SECONDS);
 
 	// Closes the file:
 	file.print("Final timestamp: ");
