555c1393c7c637ad3dadde51927b0dbd725a259c
IkerGalardi <contacto.ikergalardi@gmail.com>
2020-02-19 21:34:48 +0100

    Minor changes of organization
    The errors.h file has been moved to the folder src and the 'take_samples' function moved from main.ino to logger.h

--- logger/src/logger.h
+++ logger/src/logger.h
@@ -1,6 +1,16 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+void take_samples(HX711& scale, File& file, int sample_count)
+{
+	for (int f = 0; f < sample_count; f++)
+	{
+		file.print(scale.read());
+		file.print(":");
+	}
+}
+
+
 void logger() 
 { 
 	// Wait until the activation signal is on, and take the specified samples
--- logger/src/main.ino
+++ logger/src/main.ino
@@ -72,43 +72,24 @@ void make_signals(int signal_count, int interval)
 	}
 }
 
-void take_samples(HX711& scale, File& file, int sample_count)
-{
-	for (int f = 0; f < sample_count; f++)
-	{
-		file.print(scale.read());
-		file.print(":");
-	}
-}
 
-void logger() 
-{ 
-	// Wait until the activation signal is on, and take the specified samples
-	DEBUG_LOG_LINE("Starting sampling routine");
-	take_samples(scale, file, 80*RECORD_SECONDS);
-
-	// Closes the file:
-	file.print("Final timestamp: ");
-	file.println(millis());
-	file.close();
-	DEBUG_LOG("Program finalized"); 
-	signal_ok();
-}
 
 void setup() {
 	int res = init();
 	if(res != 0) {
-		//TODO: error check
+		// TODO: error check
 	}
 
 	res = radio();
 	while(res != 0) {
-		//TODO: pitiditos 'res' veces
+		// TODO: pitiditos 'res' veces
 	
 		res = radio();
 	}
 
 	logger();
+
+	// TODO: ALL OK
 }
 
 void loop() {
